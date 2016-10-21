4430ac086eb87f4a6cab74bf82cb7ff51f1ff309
Anant4830 <anant4830@gmail.com>
2016-10-22 00:40:01 +0530

    Add files via upload

---
+++ java_session2Assingment4/src/java_session2Assingment4/highestof3no.java
@@ -0,0 +1,20 @@
+package java_session2Assingment4;
+
+public class highestof3no {
+
+	public static void main(String[] args) {
+		int a=2,b=5,c=8;
+		int largest;
+		if(a>=b&&a>=c)
+			largest=a;
+		else if(b>=a&&b>=c)
+			largest=b;
+		else
+			largest=c;
+		System.out.println(largest);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
